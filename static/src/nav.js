883ac8d513438d8d3ac150fa23b1559aebb01ef5
Benjamin Binder <bind.ben@gmail.com>
2021-08-11 18:19:55 +0200

    Refactor javascript

--- static/src/nav.js
+++ static/src/nav.js
@@ -4,34 +4,37 @@
 // The other one to close the menu when the user has clicked a link
 // This is ususally not necessary, because a new site would be loaded
 // Because this is a single page however, we cannot rely on the hack.
-let nav = document.querySelector('body > nav');
-document.querySelector('#menu-icon').addEventListener('click', event => {
+const nav = document.querySelector('body > nav');
+document.querySelector('#menu-icon').addEventListener('click', async (event) => {
 	nav.classList.toggle('visible');
 });
 
-document.querySelectorAll('body > nav a').forEach(a => {
+
+for (const a of document.querySelectorAll('body > nav a')) {
 	a.addEventListener('click', event => {
 		nav.classList.toggle('visible');
 	});
-});
+}
+
 
 // Height adjustment of navigation bar after page scrolled down
-document.addEventListener('scroll', function (event) {
-	let header = document.querySelector('body > nav');
+document.addEventListener('scroll', async (event) => {
+	const header = document.querySelector('body > nav');
 	if (window.scrollY > 75)
 		header.classList.add('scrolled');
 	else
 		header.classList.remove('scrolled');
 });
 
-let visiblityCallback = function(entries, observer) {
-	entries.forEach(entry => {
-		let selector = 'li[target~="#' + entry.target.id + '"]';
-		let navElement = document.querySelector(selector);
 
+function visiblityCallback(entries, observer) {
+	for(const entry of entries) {
+		const selector = `li[target~="#${entry.target.id}"]`;
+		const navElement = document.querySelector(selector);
+	
 		if(navElement)
 			navElement.classList.toggle('visible');
-	});
+	}
 };
 
 // Visibility indicator in top navigation
@@ -42,11 +45,13 @@ let visiblityCallback = function(entries, observer) {
 // However, this event gets also fired on page load (why I don't know).
 // This messes up the state kept via a css-class.
 // To patch this the concerning elements have the css-class pre-applied.
-var observer = new IntersectionObserver(visiblityCallback, {
+const observer = new IntersectionObserver(visiblityCallback, {
 	root: null,
 	rootMargin: '0px',
 	threshold: 0.9
 });
 
-let sections = document.querySelectorAll('main > section');
-sections.forEach(section => observer.observe(section));
+const sections = document.querySelectorAll('main > section');
+for(const section of sections) {
+	observer.observe(section); 
+}
